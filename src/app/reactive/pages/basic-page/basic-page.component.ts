a169184653b984e997e09609ea2252e8bec074a4
Tío Gallo <juanftp100@gmail.com>
2025-12-30 13:56:34 -0300

    modularizar las validaciones en utils

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,6 +11,8 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class BasicPageComponent {
 
   private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -23,32 +26,49 @@ export class BasicPageComponent {
   //   inStorage: new FormControl(0),
   // });
 
-  isValidField(fieldName: string): boolean | null {
-    return !!this.myForm.controls[fieldName].errors;
-  }
+  // isValidField(fieldName: string): boolean | null {
+  //   return ( this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched);
+  // }
 
-  getFieldError(fieldName: string): string | null {
+  // getFieldError(fieldName: string): string | null {
 
-    if ( !this.myForm.controls[fieldName] ) return null;
+  //   if ( !this.myForm.controls[fieldName] ) return null;
 
-    const errors = this.myForm.controls[fieldName].errors ?? {};
+  //   const errors = this.myForm.controls[fieldName].errors ?? {};
 
-    for ( const key of Object.keys(errors) ) {
-      switch ( key ) {
-        case 'required':
-          return 'Este campo es requerido';
+  //   for ( const key of Object.keys(errors) ) {
+  //     switch ( key ) {
+  //       case 'required':
+  //         return 'Este campo es requerido';
 
-        case 'minlength':
-          return `Mínimo de ${ errors['minlength'].requiredLength } caracteres`;
+  //       case 'minlength':
+  //         return `Mínimo de ${ errors['minlength'].requiredLength } caracteres`;
 
-        case 'min':
-          return `Valor mínimo de ${ errors['min'].min } caracteres`;
+  //       case 'min':
+  //         return `Valor mínimo de ${ errors['min'].min } caracteres`;
 
-      }
+  //     }
+  //   }
+
+  //   return null;
+
+  // }
+
+  onSave() {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
     }
 
-    return null;
+    console.log(this.myForm.value);
+    this.myForm.reset();
 
+    // por defecto
+    // this.myForm.reset({
+    //   price: 100,
+    //   inStorage: 50,
+    //   name: 'Juancito'
+    // });
   }
 
 }
